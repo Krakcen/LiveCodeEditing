f77f13d0de2dd7b091854a81c9ae971edab2540c
bernel_m <marceau.bernelin@epitech.eu>
2016-08-25 21:59:54 +0200

    modified client connection

--- start.js
+++ start.js
@@ -3,6 +3,7 @@ var http = require('http');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
+var dataOnConnect = "";
 
 app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res)
@@ -14,10 +15,15 @@ io.on('connection', function (socket)
 {
 	console.log('Un client est connecté !');
 	
+	io.sockets.emit('data', dataOnConnect);
+	
 	socket.on('data', function (data)
 	{
+		dataOnConnect = data;
 		io.sockets.emit('data', data);
 	});
 });
+
+
 
 server.listen(8888);
