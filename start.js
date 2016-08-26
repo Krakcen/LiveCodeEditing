5235887e5a19b1505fad4cec415f4d6c53cfa2fa
bernel_m <marceau.bernelin@epitech.eu>
2016-08-26 17:21:35 +0200

    better editor

--- start.js
+++ start.js
@@ -10,6 +10,11 @@ app.get('/', function(req, res)
 {
 	res.sendFile(__dirname + '/public/index.html');
 });
+/*
+.get('/:id', function(req, res)
+{
+	res.sendFile(__dirname + '/public/room.html?id=' + req.params.id);
+});*/
 
 io.on('connection', function (socket)
 {
